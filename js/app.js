ef126544625331717dc4d5f150a4d410136c799e
Irina Kramer <irakramer@gmail.com>
2020-07-11 13:20:53 -0700

    keys edits

--- js/app.js
+++ js/app.js
@@ -5,38 +5,12 @@ $(document).ready(function () {
    * ============ VARIABLES AND FUNCTIONS ============ 
    */
 
-  // Reddit, Bing, Patch for local, 
-
   // API URLs
   const proxyURL = 'https://boiling-inlet-94554.herokuapp.com/';
   const mashableUrl = 'https://api.mashable.com/v1/posts/';
   const nytUrl = 'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=' + FEEDR_NYT_API;
   const newsapiUrl = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + FEEDR_NEWS_API;
   const bingUrl = 'https://feedr-bingnews.cognitiveservices.azure.com/bing/v7.0/news';
-
-  console.log(FEEDR_NYT_API);
-  // function handleDefaultResponse(allData) {
-  //   $.each(allData, function () {
-  //     if (this.title !== '') {
-  //       // Define DOM elements to build an article         
-  //       let $image = $('<img>').attr('src', this.images.i120x120);
-  //       let $link = $('<a>').attr('href', this.link);
-  //       let $title = $('<h3>').text(this.title);
-  //       let $topic = $('<h6>').text(this.key_topics);
-  //       let $author = $('<small>').text(this.author);
-  //       let $description = $('<p>').text(this.content.excerpt).addClass('hidden');
-  //       let MashableObj = {
-  //         image: $image,
-  //         link: $link,
-  //         title: $title,
-  //         author: $author,
-  //         description: $description
-  //       }
-  //       appendToDom(MashableObj);
-  //     }
-  //   });
-  //   displayArticle();
-  // }
 
   // function handleDefaultResponse(allData) {
   //   $.each(allData, function () {
@@ -184,24 +158,6 @@ $(document).ready(function () {
       }
     });
   }
-
-  // Default feed - Mashable 
-  // function getDefault() {
-  //   $.ajax({
-  //     url: nytAllUrl,
-  //     data: {
-  //       format: "json"
-  //     },
-  //     error: function () {
-  //       $('#main').html('<p>An error has occurred. </p>');
-  //       //$('#main').html('<p>An error has occurred. </p>' + textStatus + ' ' + errorThrown);
-  //     },
-  //     success: function (response) {
-  //       handleDefaultResponse(response.results);
-
-  //     }
-  //   });
-  // }
 
   /**
    *  ============ PROGRAM EXECUTION ============ 
